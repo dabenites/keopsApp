a5d39b28d3883f13ec78b0ee29ec0735fda05ce1
David <dbenites@renelagos.com>
2021-11-06 22:26:45 -0300

    carga
    carga

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    //res.render('index');
+    //res.redirect('/signin2'); 
+    //console.log("asda");
+    res.send('asd');
+});
+
+module.exports = router;
